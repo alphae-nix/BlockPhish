cc0208039a398d8ca96c7367cf8a73c2a85c7316
CappiLucky <46004658+CappiLucky@users.noreply.github.com>
2021-05-21 19:43:18 +0200

    ajout de la routine d'ecoute

---
+++ extension/background.js
@@ -0,0 +1,9 @@
+//script d'arriere plan
+//Chrome analyse le fichier spécifié pour obtenir des instructions supplémentaires, telles que les événements importants qu'il doit écouter
+let color = '#FFFFF'; //variable couleur
+
+//ajout d'une routine d'ecoute
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
