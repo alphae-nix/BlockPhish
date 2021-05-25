0a9d9504b15c5574e8b97c593d74d8743fb77a98
alphae-nix <18laurent@gmail.com>
2021-05-25 13:21:49 +0200

    ✅ récupère l'url et nous renvois la probabilité qu'il soit compromis

--- deep_learning/mainFunc.py
+++ deep_learning/mainFunc.py
@@ -8,31 +8,29 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
 
-# def main():
-#     objet = urlToArray.urlToArray(sys.argv[1]).getArray()
-#     print(objet)
-#     return objet
+def main():
+     objet = urlToArray.urlToArray(sys.argv[1]).getArray()
+     print(objet)
+     print(objet.shape)
+     
+     cmd = "wget -nc https://perso.esiee.fr/~delattel/projetE3/model.json https://perso.esiee.fr/~delattel/projetE3/model.h5"
+     os.system(cmd)
 
-# if __name__ == "__main__":
-#     main()
+     # load json and create model
+     json_file = open('model.json', 'r')
+     loaded_model_json = json_file.read()
+     json_file.close()
+     loaded_model = model_from_json(loaded_model_json)
 
-cmd = "wget -nc https://perso.esiee.fr/~delattel/projetE3/model.json https://perso.esiee.fr/~delattel/projetE3/model.h5"
-os.system(cmd)
+     # load weights into new model
+     loaded_model.load_weights("model.h5")
+     print("Loaded model from disk") 
 
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
+     # prédiction à partir du modèle
+     pred=loaded_model.predict(objet)
+     print(pred)
 
+     return pred
 
-url = "http://tinyurl.com/yjp5stx2"
-
-objet = urlToArray.urlToArray(url)
-print(objet.getArray()) 
-
-pred=model.predict(objet.getArray())
-print(pred)
+if __name__ == "__main__":
+     main()
