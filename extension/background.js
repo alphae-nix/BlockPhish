6c592ac671f679745ef90b3b05e4ec7ba79c0f3e
CappiLucky <46004658+CappiLucky@users.noreply.github.com>
2021-06-01 14:09:15 +0200

    modif pour le meilleur

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,8 @@
 //script d'arriere plan
 //Chrome analyse le fichier spécifié pour obtenir des instructions supplémentaires, telles que les événements importants qu'il doit écouter
-let color = '#FFFFF'; //variable couleur
 
 //ajout d'une routine d'ecoute
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.storage.sync.set({ "StoredPrecision": 0.8 });
 });
 
