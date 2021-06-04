538cfe9c1919edc766490ea35f9e027f470d8b7d
blackjack-nix <45201732+blackjack-nix@users.noreply.github.com>
2021-06-04 22:30:19 +0200

    edit return tab

--- back/app.py
+++ back/app.py
@@ -1,7 +1,7 @@
 from flask import request, Flask, jsonify
 import json 
 import urlToArray
-
+import time
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
@@ -11,19 +11,18 @@ app = Flask(__name__)
 
 def init_ia():
     # load json and create model
-     json_file = open("./deep_learning/model-15.json", 'r')
-     loaded_model_json = json_file.read()
-     json_file.close()
-     loaded_model = model_from_json(loaded_model_json)
+    json_file = open("./deep_learning/model-15.json", 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
 
      # load weights into new model
-     loaded_model.load_weights("./deep_learning/model-15.h5")
-     print("Loaded model from disk") 
-     return loaded_model
+    loaded_model.load_weights("./deep_learning/model-15.h5")
+    return loaded_model
 
 def predict(model, array):
-     pred=model.predict([array])
-     return pred
+    pred=model.predict([array])
+    return pred
 
 model = init_ia()
 
@@ -33,9 +32,12 @@ def test():
     urls = mydata.split(",")
     preds = []
     for url in urls:
-        preds.append(predict(model,urlToArray.urlToArray(url).getArray()))
+        array = urlToArray.urlToArray(url).getArray()        
+        preds.append(predict(model,array)[0][0])
+
 
     print(preds)
-    resp = jsonify(str(preds[0]))
+
+    resp = jsonify(str(preds))
     resp.headers['Access-Control-Allow-Origin']='*'
     return resp
