0d99dbc03ac6bdc0626003993f00650872ff27d6
blackjack-nix <45201732+blackjack-nix@users.noreply.github.com>
2021-06-04 15:01:44 +0200

    Update app.py

--- back/app.py
+++ back/app.py
@@ -11,19 +11,18 @@ app = Flask(__name__)
 
 def init_ia():
     # load json and create model
-     json_file = open('../deep_learning/model.json', 'r')
+     json_file = open("./deep_learning/model-15.json", 'r')
      loaded_model_json = json_file.read()
      json_file.close()
      loaded_model = model_from_json(loaded_model_json)
 
      # load weights into new model
-     loaded_model.load_weights("../deep_learning/model.h5")
+     loaded_model.load_weights("./deep_learning/model-15.h5")
      print("Loaded model from disk") 
      return loaded_model
 
 def predict(model, array):
-     pred=model.predict(array)
-     print(pred)
+     pred=model.predict([array])
      return pred
 
 model = init_ia()
@@ -31,11 +30,12 @@ model = init_ia()
 @app.route('/test', methods=['POST'])
 def test():
     mydata = request.data.decode(encoding="UTF-8")
-    array = mydata.split(",")
-    array2 = urlToArray.urlToArray(array[0]).getArray()
-    pred = predict(model,array2)
-    print(pred)
+    urls = mydata.split(",")
+    preds = []
+    for url in urls:
+        preds.append(predict(model,urlToArray.urlToArray(url).getArray()))
 
-    resp = jsonify(pred)
+    print(preds)
+    resp = jsonify(str(preds[0]))
     resp.headers['Access-Control-Allow-Origin']='*'
     return resp
