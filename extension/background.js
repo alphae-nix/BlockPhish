b988e8d7beebc7d954a866c71d3b39e0614fdecf
blackjack-nix <45201732+blackjack-nix@users.noreply.github.com>
2021-06-09 14:13:41 +0200

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,7 @@
 //script d'arriere plan
 //Chrome analyse le fichier spécifié pour obtenir des instructions supplémentaires, telles que les événements importants qu'il doit écouter
 
-//ajout d'une routine d'ecoute
-chrome.runtime.onInstalled.addEventListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ 'StoredPrecision': 80 }, function() {
-  console.log("ok");
   });
 });
